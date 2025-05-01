75836e4c368f157fdc1cc8a4f62c1de9217228d3
Higanbana-t <truongmhtt@gmail.com>
2025-05-01 23:07:55 +0700

    Theming

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from '@mui/material/Button'
-
-
+import Typography from '@mui/material/Typography'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Typography variant="h1" color="text.secondary">Theme</Typography>
       <Button variant="contained">Hello world</Button>
 
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.js'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
