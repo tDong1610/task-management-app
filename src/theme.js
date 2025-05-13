317656f0066f23f4609cf7655cbc0665bd8ba59d
Higanbana-t <truongmhtt@gmail.com>
2025-05-14 01:02:18 +0700

    Menu2

--- src/theme.js
+++ src/theme.js
@@ -21,6 +21,64 @@ const theme = extendTheme({
         secondary: orange
       }
     }
+  },
+  components: {
+    MuiCssBaseline:{
+      styleOverrides: {
+        body:{
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor:'#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#00b894',
+
+          }
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          return {
+            color: theme.palette.primary.main,
+            fontSize: '0.875rem'
+          }
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          return {
+            color: theme.palette.primary.main,
+            fontSize: '0.875rem',
+            '.MuiOutlinedInput-notchedOutline':{
+              borderColor: theme.palette.primary.light
+            },
+            '&:hover':{
+              '.MuiOutlinedInput - notchedOutline':{
+                borderColor: theme.palette.primary.main
+              }
+            },
+            '& fieldset':{
+              borderWidth:'1px !important'
+            }
+          }
+        }
+      }
+    }
   }
 })
 
